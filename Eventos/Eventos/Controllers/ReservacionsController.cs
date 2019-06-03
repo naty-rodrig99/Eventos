e0f9d1cd0af2e8eedb02f78a6cc6ccacd0ccc430
eriraldo <ericksalazar_g@hotmail.com>
2019-06-02 23:35:55 -0600

    reserva arreglada

--- Eventos/Eventos/Controllers/ReservacionsController.cs
+++ Eventos/Eventos/Controllers/ReservacionsController.cs
@@ -5,24 +5,24 @@ using System.Data.Entity;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Net;
+using System.Net.Mail;
 using System.Web;
 using System.Web.Mvc;
 using Eventos.Models;
-using System.Net.Mail;
 
 namespace Eventos.Controllers
 {
-    public class ReservacionesController : Controller
+    public class ReservacionsController : Controller
     {
         private ESeventosEntities1 db = new ESeventosEntities1();
 
-        // GET: Reservaciones
+        // GET: Reservacions
         public ActionResult Index()
         {
             return View(db.Reservacions.ToList());
         }
 
-        // GET: Reservaciones/Details/5
+        // GET: Reservacions/Details/5
         public ActionResult Details(int? id)
         {
             if (id == null)
@@ -37,29 +37,30 @@ namespace Eventos.Controllers
             return View(reservacion);
         }
 
-        // GET: Reservaciones/Create
+        // GET: Reservacions/Create
         public ActionResult Create()
         {
             return View();
         }
 
-        // POST: Reservaciones/Create
+        // POST: Reservacions/Create
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create(Reservacion reservacion)
+        public ActionResult Create([Bind(Include = "idReservacion,numReservacion,fecha,hora,descripcion,idPaquete")] Reservacion reservacion)
         {
-            String connection = "Server =.; Database = ESeventos; Trusted_Connection = True";
-            SqlConnection conn = new SqlConnection(connection);
-            String query = "exec SP_RealizarReservacion " + reservacion.numReservacion + ", '" + reservacion.fecha + reservacion.hora + ", '" + reservacion.descripcion + ", '" + reservacion.idPaquete + ", '" + "'";
-            SqlCommand cmd = new SqlCommand(query, conn);
-            conn.Open();
+            if (ModelState.IsValid)
+            {
+                db.Reservacions.Add(reservacion);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
 
-            return View("~/Views/Reservaciones/Index.cshtml");
+            return View(reservacion);
         }
 
-        // GET: Reservaciones/Edit/5
+        // GET: Reservacions/Edit/5
         public ActionResult Edit(int? id)
         {
             if (id == null)
@@ -74,12 +75,12 @@ namespace Eventos.Controllers
             return View(reservacion);
         }
 
-        // POST: Reservaciones/Edit/5
+        // POST: Reservacions/Edit/5
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "idReservacion,numReservacion,fecha,hora,descripcion")] Reservacion reservacion)
+        public ActionResult Edit([Bind(Include = "idReservacion,numReservacion,fecha,hora,descripcion,idPaquete")] Reservacion reservacion)
         {
             if (ModelState.IsValid)
             {
@@ -90,7 +91,7 @@ namespace Eventos.Controllers
             return View(reservacion);
         }
 
-        // GET: Reservaciones/Delete/5
+        // GET: Reservacions/Delete/5
         public ActionResult Delete(int? id)
         {
             if (id == null)
@@ -105,7 +106,7 @@ namespace Eventos.Controllers
             return View(reservacion);
         }
 
-        // POST: Reservaciones/Delete/5
+        // POST: Reservacions/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public ActionResult DeleteConfirmed(int id)
@@ -125,19 +126,18 @@ namespace Eventos.Controllers
             base.Dispose(disposing);
         }
 
-        [HttpPost]
-        public ActionResult Reservar(Reservacion reservacion )
+        public ActionResult Reservar(Reservacion reservacion)
         {
             String connection = "Server=.;Database=ESeventos;Trusted_Connection=True";
             SqlConnection conn = new SqlConnection(connection);
-            String query = "exec SP_RealizarReservacion " + reservacion.numReservacion + ", '" + reservacion.fecha +reservacion.hora + ", '" + reservacion.descripcion + ", '" + reservacion.idPaquete+ ", '" + "'";
+            String query = "exec SP_RealizarReservacion " + reservacion.numReservacion + ", '" + reservacion.fecha + reservacion.hora + ", '" + reservacion.descripcion + ", '" + reservacion.idPaquete + ", '" + "'";
             SqlCommand cmd = new SqlCommand(query, conn);
             conn.Open();
-            correoReservaCliente(HomeController.mail, (int) reservacion.idPaquete, reservacion.idReservacion, reservacion.descripcion);
+            //correoReservaCliente(HomeController.mail, (int) reservacion.idPaquete, reservacion.idReservacion, reservacion.descripcion);
             return View("~/Views/Home/Administrador.cshtml");
         }
 
-        public void correoReservaCliente(String mail,int idPaquete,int idReserva, String descripcion)
+        public void correoReservaCliente(String mail, int idPaquete, int idReserva, String descripcion)
         {
 
             MailMessage correo = new MailMessage("noreply@es-eventos.com", mail);
@@ -147,7 +147,7 @@ namespace Eventos.Controllers
             client.UseDefaultCredentials = false;
             client.Host = "smtp.gmail.com";
             correo.Subject = "Notificacion de reserva.";
-            String body = "Estimado cliente, le informamos que se ha reservado un paquete a su nombre con los siguientes datos:\n\nNumero de reservacion:" + idReserva + "\n\nID del Paquete:" + idPaquete + "\n\nDescripcion:" + descripcion+"\n\n";
+            String body = "Estimado cliente, le informamos que se ha reservado un paquete a su nombre con los siguientes datos:\n\nNumero de reservacion:" + idReserva + "\n\nID del Paquete:" + idPaquete + "\n\nDescripcion:" + descripcion + "\n\n";
             body += "Si usted no realizo esta reservacion , favor ponerse en contacto con soporte tecnico.\nSaludos!\n\nEl Equipo de E|S Eventos";
             correo.Body = body;
             client.Send(correo);
